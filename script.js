24f36ca4d90145b5f7835c4993164199e0328748
Johnrasc <mulingbayanjohnrasc71@gmail.com>
2025-10-05 13:56:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("alertButton");
+
+  btn.addEventListener("click", () => {
+    alert("Welcome to VOX VERITAS!");
+  });
+});
